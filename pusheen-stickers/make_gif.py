14f132dbb8cc9ffda4a4cc09b551a371d9a661b0
Emma Yeap <emmacyeap@gmail.com>
2018-11-21 13:58:12 +0800

    Pusheen stickers now in a folder called pusheen

--- pusheen-stickers/make_gif.py
+++ pusheen-stickers/make_gif.py
@@ -1,23 +1,27 @@
 from PIL import Image
 import os
 
-if not os.path.exists('gif'):
-    os.makedirs('gif')
 os.system('rm -r tmp')
 os.makedirs('tmp')
 
-def gifify(png, path_prefix='png/'):
+def gifify(png, path_prefix, delay=20):
     """
     Converts the png at the path within folder path_prefix, into small and large animated gifs.
 
-    The number of divisions to split the ping into (n_w, n_h, width-wise and height-wise) is read
+    The number of divisions to split the png into (n_w, n_h, width-wise and height-wise) is read
     out of the png name (ie image_w_h.png)
+
+    Delay controls the milliseconds between each frame in the gif.
     """
     im = Image.open(path_prefix + png)
     path_components = png.rstrip('.png').split('_')
     n_w = int(path_components[-2])
     n_h = int(path_components[-1])
     final_name = '_'.join(path_components[:-2])
+
+    out_folder_name = path_prefix.rstrip('/') + '_gif'
+    if not os.path.exists(out_folder_name):
+        os.makedirs(out_folder_name)
 
     # we want to split it into n_w * n_h equal segments
     w, h = im.size
@@ -49,20 +53,20 @@ def gifify(png, path_prefix='png/'):
                 os.system('convert tmp/{n}.gif -resize {w}x{h} -unsharp 0x0.75+0.75+0.008 tmp/{n}_small.gif'.format(
                     n=segment_name, w=segment_w//1.75, h=segment_h//1.75))
 
+    args = (delay, out_folder_name, final_name)
     # make small version
-    os.system('convert -delay 20 -dispose previous -loop 0 tmp/*_small.gif gif/%s_small.gif' % final_name)
+    os.system('convert -delay %s -dispose previous -loop 0 tmp/*_small.gif %s/%s_small.gif' % args)
     os.system('rm tmp/*small.gif')
     # make big version
-    os.system('convert -delay 20 -dispose previous -loop 0 tmp/*.gif gif/%s.gif' % final_name)
+    os.system('convert -delay %s -dispose previous -loop 0 tmp/*.gif %s/%s.gif' % args)
 
     os.system('rm tmp/*png && rm tmp/*gif')
 
 # make pusheen gifs
-pngs = [f for f in os.listdir('png') if f.endswith('.png')]
-print pngs
-for png in pngs:
-    gifify(png)
-
+pusheen_pngs = [f for f in os.listdir('pusheen') if f.endswith('.png')]
+print pusheen_pngs
+for png in pusheen_pngs:
+    gifify(png, path_prefix='pusheen/')
 
 # cleanup
 os.system('rm -r tmp')
